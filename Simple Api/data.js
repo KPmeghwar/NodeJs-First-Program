2fa0cffa461f3a0a37db2c87f008a59f59cbd73f
Kailash Meghwar <premanikailash97@gmail.com>
2022-05-22 17:24:37 +0500

    simple Api Nodejs

---
+++ Simple Api/data.js
@@ -0,0 +1,8 @@
+const data=[
+    {Name:'Kailash Meghwar',email:'kailash@gmail.com'},
+    {Name:'Ahmed',email:'ahmed@gmail.com'},
+    {Name:'Ali ',email:'ali@gmail.com'},
+    {Name:'Toto Meghwar',email:'toto@gmail.com'}
+];
+
+module.exports=data;
