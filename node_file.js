e16bc1017118fab2067b740f56a46c5b8b0eb104
Kailash Meghwar <premanikailash97@gmail.com>
2022-05-22 13:30:36 +0500

    Nodejs First Program

---
+++ node_file.js
@@ -0,0 +1,12 @@
+// const http=require('http');
+
+// http.createServer((req,resp)=>{
+//  resp.write('<h1>hello World</h1>');
+//  resp.end()
+// }).listen(4500)
+
+// function abc(req,res){
+//     res.write("Hello Kailash Meghwar")
+//     res.end()
+// }
+// http.createServer(abc).listen(3001);
